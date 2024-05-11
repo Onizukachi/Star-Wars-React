35101f2f0b451da89f6cbcb190ae7b8093e1e385
Alexei <dark_sao@mail.ru>
2024-05-11 17:11:37 +0300

    test deploy 3

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -6,7 +6,7 @@ import styles from './App.module.css';
 const App = () => {
   return (
     <>
-      <BrowserRouter basename='/Star-Wars-React/'>
+      <BrowserRouter basename='/Star-Wars-React'>
         <div className={styles.wrapper}>
           <Header />
           
