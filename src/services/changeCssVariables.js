afa1673233465fa5f7bbef7feb6dfd703ec3afff
Alexei <dark_sao@mail.ru>
2024-05-11 16:28:28 +0300

    init commit

--- src/services/changeCssVariables.js
+++
@@ -1,9 +0,0 @@
-export const changeCssVariables = (theme) => {
-    const root = document.documentElement;
-    
-    const cssVariables = ['header', 'bgimage']
-
-    cssVariables.forEach((el) => {
-        root.style.setProperty(`--theme-default-${el}`, `var(--theme-${theme}-${el})`)
-    })
-}
--- src/store/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import favoriteReducer from "./favoriteReducer";
-
-export default combineReducers({
-    favoriteReducer
-})
